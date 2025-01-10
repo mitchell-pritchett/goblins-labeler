3e3fe83a1d9521d0b3b589994cef5e570ec6ff7a
mitchell-pritchett <kangkyuran7@gmail.com>
2025-01-10 03:27:50 -0800

    Ready for deploy

--- goblins-backend/index.js
+++ goblins-backend/index.js
@@ -1,20 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const imagesRoutes = require("./routes/images");
 const annotationsRoutes = require("./routes/annotations");
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
-// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-// Routes
+// API Routes
 app.use("/api/images", imagesRoutes);
 app.use("/api/annotations", annotationsRoutes);
+
+// serving React frontend files
+app.use(express.static(path.join(__dirname, "../goblins-frontend/build")));
+
+// for non-API routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../goblins-frontend/build", "index.html"));
+});
 
 // Start server
 app.listen(PORT, () => {
